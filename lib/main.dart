22b1d78438c9a7aefb4920134febfa90e3c6eb6e
Ananth Ramaswamy <ananthramaswamy@wirelessprv-10-192-148-64.near.illinois.edu>
2023-10-13 17:39:29 -0500

    set up home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,13 +28,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  File? _photo;
+
+  Future _takePhoto() async {
+    // allow the user to take a photo with their camera
+  }
+
+  Future _selectPhoto() async {
+    // allow the user to pick a photo from their image gallery
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              key: const Key('photo_display'),
+              margin: const EdgeInsets.all(8),
+              width: screenSize.width,
+              height: screenSize.height * 0.3,
+              color: Colors.grey[300],
+              child: const Center(
+                child: Placeholder(), // replace this line with the logic for displaying the selected photo
+              ),
+            ),
+            const SizedBox(height: 150),
+            SizedBox(
+              width: screenSize.width * 0.5,
+              height: screenSize.height * 0.05,
+              child: ElevatedButton(
+                key: const Key('upload_photo_btn'),
+                onPressed: _takePhoto, 
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.image_outlined),
+                      SizedBox(width: 10),
+                      Text('Select Photo'),
+                    ],
+                  ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: screenSize.width * 0.5,
+              height: screenSize.height * 0.05,
+              child: ElevatedButton(
+                key: const Key('take_photo_btn'),
+                onPressed: _selectPhoto, 
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.photo_camera),
+                      SizedBox(width: 10),
+                      Text('Take Photo'),
+                    ],
+                  ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bird_guide/main.dart';
-
 void main() {
-  testWidgets('Widget tests', (WidgetTester tester) async {
-    // code goes here
+  testWidgets('Home screen widget test', (WidgetTester tester) async {
+    Finder photoDisplayFinder = find.byKey(const Key('photo_display'));
+    Finder uploadPhotoBtnFinder = find.byKey(const Key('upload_photo_btn'));
+    Finder takePhotoBtnFinder = find.byKey(const Key('take_photo_btn'));
+
+    /* 1. Make sure that all the widgets are present on the home screen */
+    // Use the following: expect(<finder>, findsOneWidget);
   });
 }
