011e9107d10398df8b434d59bdbdbc51ec41075e
Ananth Ramaswamy <ananthramaswamy@ananths-mbp.lan>
2023-11-24 13:47:28 -0800

    added top bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +11,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
 }
 
@@ -23,6 +27,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/project_code_logo.png',
+                scale: 1.5,
+              ),
+              const SizedBox(width: 15),
+              const Text(
+                'Bird Guide',
+                style: TextStyle(
+                  fontFamily: 'Work Sans',
+                  fontSize: 30,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      ),
+    );
   }
 }
